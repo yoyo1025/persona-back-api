793b7f86a8770585886b6fe002f2ebe84e368a5d
nemoken <bp22092@shibaura-it.ac.jp>
2024-09-28 15:12:43 +0900

    ペルソナDBの構造体を定義

--- database/persona.go
+++ database/persona.go
@@ -7,3 +7,20 @@ var openaiClient *openai.Client
 func SetOpenAIClient(client *openai.Client) {
 	openaiClient = client
 }
+
+import(
+	"encoding/json"
+	"fmt"
+)
+
+type Persona struct{
+	Name string `json:"name"`
+	Sex	string `"json:sex"`
+	Age	int `"json:age"`
+	Profession string `"json:profession"`
+	Problems string `"json:problems"`
+	Behavior string `"json:behavior"`
+}
+
+
+
