16caaf8f09034bb219ee3aeb91add38548689e18
nemoken <bp22092@shibaura-it.ac.jp>
2024-09-28 16:04:01 +0900

    ハンドラー関数の定義及び通信内容の取得機能実装

--- database/persona.go
+++ database/persona.go
@@ -22,5 +22,26 @@ type Persona struct{
 	Behavior string `"json:behavior"`
 }
 
+//通信処理
+func InputPersona(w http.ResponseWriter, r *http.Request){
+	//getメソッド以外をはじく
+	if r.Method != http.MethodGet {
+		http.Error(w, "許可されていないメソッドです", http.StatusMethodNotAllowed)
+		return
+	}
+	
+	//ペルソナ型の構造体を使用
+	var persona Persona
+// リクエストボディからJSONデータをデコード
+	err := json.NewDecoder(r.Body).Decode(&persona)
+	if err != nil {
+		http.Error(w, "リクエストデータのデコードに失敗しました", http.StatusBadRequest)
+		return
+	}
+	if persona.Name == "" || persona.Sex == "" || persona.Age == "" || persona.Problems =="" || persona.Behavior == "" || persona.Profession == "" {
+		http.Error(w, "必要なフィールドが不足しています", http.StatusBadRequest)
+		return
+	}
 
+}
 
